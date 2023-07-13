8e24ae6fef08f09dbd074e2fd73b9305a1fa3471
Samriddhi Shreyasee <112820579+Samiriddhi@users.noreply.github.com>
2023-07-13 12:32:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,19 +1,31 @@
 function threeSum(S, target) {
 // write your code here
-	  let sum=0;
-	 
-	for(let i=0;i<S.length;i++){
-		for(let j=i+1;j<S.length-1;j++){
-		    for(let k=j+1;k<S.length-2;k++){
-				 sum+=S[i]+[j]+[k];
-				if(sum>=target){
-					 return sum;
-				}
-			}
-		}
-	}
-	
-  
+	  
+	 arr.sort((a,b)=>a-b)
+
+        // console.log(arr)
+        let len = arr.length
+        let closestSum = 10000000;
+        for(let i=0;i<=len-3;i++){
+            // len-1
+            // len-2
+            // len-3
+            let left = i+1;
+            let right = len-1;
+
+            while(left<right){
+                let currSum = arr[i]+arr[left]+arr[right]
+
+                if(Math.abs(currSum-target)<Math.abs(closestSum-target))
+                    closestSum = currSum
+
+                if(currSum>target)
+                    right--
+                else
+                    left++
+            }
+        }
+        return closestSum;
 }
 
 module.exports = threeSum;
