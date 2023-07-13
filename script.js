22e574ee2fe9b56a2f8eac33d4354db2ff19371c
Samriddhi Shreyasee <112820579+Samiriddhi@users.noreply.github.com>
2023-07-13 12:27:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
-function threeSum(arr, target) {
+function threeSum(S, target) {
 // write your code here
+	  let sum=0;
+	 
+	for(let i=0;i<S.length;i++){
+		for(let j=i+1;j<S.length-1;j++){
+		    for(let k=j+1;k<S.length-2;k++){
+				 sum+=S[i]+[j]+[k];
+				if(sum>=target){
+					 return sum;
+				}
+			}
+		}
+	}
+	
   
 }
 
